46322c90fd45a743df03e55810a91d93067a5655
Blaise Buckland <bucklandblaise@gmail.com>
2022-08-26 20:35:34 +0100

    First version of filters

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,38 +1,32 @@
 import React from 'react'
 import styles from "./Filters.module.scss"
 
-const Filters = () => {
-  const filterItems = [
-    {
-      id: "abv",
-      label: "High ABV (> 6%)"
-    },
-    {
-      id: "classic",
-      label: "Classic range"
-    },
-    {
-      id: "acidic",
-      label: "Acidic (ph < 4)"
-    },
-    
-  ]
+// to make these work we need a toggle function and state for checked/unchece=ked for each filter.
+// use api amendments
+// one filter at a time, make so only one can be checked
 
-  const createFilters = filterItems.map((filterItem) => {
-    return (
-    <> 
-    <div className={styles.filterItem}>
-    <label for={filterItem.id}>{filterItem.label}</label>
-      <input type="checkbox" id={filterItem.id} />
-    </div>
-    </>
-    )
-  })
+const Filters = (props) => {
+  const { setAbvFilterOn, setClassicFilterOn, setAcidicFilterOn, abvFilterOn, classicFilterOn, acidicFilterOn } = props;
+
   
   return (
-    <div className={styles.filterContainer}>
-      {createFilters}
-    </div>
+    // <div className={styles.filterContainer}>
+    //   {createFilters}
+    // </div>
+    <>
+      <div className={styles.filterItem} >
+        <label for="abv">High ABV (> 6%)</label>
+        <input type="checkbox" id="abv" onClick={() => {setAbvFilterOn(!abvFilterOn)}} />
+      </div>
+      <div className={styles.filterItem} >
+        <label for="classic">Clasic range</label>
+        <input type="checkbox" id="classic" onClick={() => {setClassicFilterOn(!classicFilterOn)}} />
+      </div>
+      <div className={styles.filterItem} >
+        <label for="acidic">Acidic (ph below 4)</label>
+        <input type="checkbox" id="acidic" onClick={() => {setAcidicFilterOn(!acidicFilterOn)}} />
+      </div>
+    </>
   )
 }
 
