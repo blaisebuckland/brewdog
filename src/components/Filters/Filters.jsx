4945dd7f4bb067e1c090bace37ce861e972dc6a3
Blaise Buckland <bucklandblaise@gmail.com>
2022-08-26 13:47:15 +0100

    Add filters to NavBar

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -2,8 +2,37 @@ import React from 'react'
 import styles from "./Filters.module.scss"
 
 const Filters = () => {
+  const filterItems = [
+    {
+      id: "abv",
+      label: "High ABV (> 6%)"
+    },
+    {
+      id: "classic",
+      label: "Classic range"
+    },
+    {
+      id: "acidic",
+      label: "Acidic (ph < 4)"
+    },
+    
+  ]
+
+  const createFilters = filterItems.map((filterItem) => {
+    return (
+    <> 
+    <div className={styles.filterItem}>
+    <label for={filterItem.id}>{filterItem.label}</label>
+      <input type="checkbox" id={filterItem.id} />
+    </div>
+    </>
+    )
+  })
+  
   return (
-    <div>Filters</div>
+    <div className={styles.filterContainer}>
+      {createFilters}
+    </div>
   )
 }
 
