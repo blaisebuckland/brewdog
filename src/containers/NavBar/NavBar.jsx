ed51fd1b5271f6e31bfca066736a51428448eae0
Blaise Buckland <bucklandblaise@gmail.com>
2022-08-26 13:14:20 +0100

    Render SearchBar and Filters in NavBar

--- src/containers/NavBar/NavBar.jsx
+++ src/containers/NavBar/NavBar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import styles from "./NavBar.module.scss"
 
+import SearchBar from "../../components/SearchBar"
+import Filters from "../../components/Filters"
+
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <>
+    <section className={styles.navContainer}>
+      <SearchBar />
+      <Filters />
+    </section>
+    </>
   )
 }
 
